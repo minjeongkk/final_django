9a9837d11a9568e3617efe603a8a48473c58857e
minjeongkk <kunvx@naver.com>
2022-06-11 17:32:39 +0900

    commit

--- mywebsite/myfirstapp/views.py
+++ mywebsite/myfirstapp/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    text_var="This is my first Django app web page!!"
-    return HttpResponse(text_var)
+    context={
+
+    }
+    return render(request, "index.html",context=context)
